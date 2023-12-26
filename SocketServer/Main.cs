cda08ec5ac1d5bcc7c6491ed1bfd03760d212659
afish665 <149894985+afish665@users.noreply.github.com>
2023-12-26 22:48:43 +0800

    Add files via upload

--- SocketServer/Main.cs
+++ SocketServer/Main.cs
@@ -1,4 +1,6 @@
-﻿using MEC;
+﻿using Exiled.API.Features;
+using Exiled.API.Interfaces;
+using MEC;
 using PluginAPI.Core;
 using PluginAPI.Core.Attributes;
 using PluginAPI.Enums;
@@ -10,47 +12,53 @@ using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
+using Log = Exiled.API.Features.Log;
+using Player = Exiled.API.Features.Player;
+using Server = Exiled.API.Features.Server;
 
 namespace SocketServer
 {
-    public class Config
+    public class Config : IConfig
     { 
         public bool CXEnable { get; set; } = true;
         public int TcpPort { get; set; } = 7778;
         public string IP { get; set; } = "127.0.0.1";
+        bool IConfig.IsEnabled { get; set; }= true;
+        bool IConfig.Debug { get; set; } = false;
     }
-    public class Main
+    public class Main:Plugin<Config>
     {
-
+        public static Main GetMain;
         public CoroutineHandle coroutine { get; set; } = new CoroutineHandle();
 
-        [PluginEntryPoint("SocketServer", "1.0.0", "aaaaaaa.", "Tyiuc")]
-        void LoadPlugin()
+
+        public override void OnEnabled()
         {
+            GetMain = this;
             Log.Info("Loaded plugin, register events...");
-            EventManager.RegisterEvents(this);
+            Exiled.Events.Handlers.Server.WaitingForPlayers += this.Wait;
+            base.OnEnabled();
         }
-
-    
-
-
-        [PluginEvent(ServerEventType.WaitingForPlayers)]
-        void Wait() 
+       public void Wait() 
         {
             if (coroutine.IsRunning)
             {
                 Timing.KillCoroutines(coroutine);
                 coroutine = Timing.RunCoroutine(SocketServer());
+                Log.Debug("Started");
             }
-            coroutine = Timing.RunCoroutine(SocketServer());
-            Log.Debug("Started");
+            else
+            {
+                coroutine = Timing.RunCoroutine(SocketServer());
+                Log.Debug("Started");
+            }
         }
 
         public static IEnumerator<float> SocketServer()
         {
             yield return Timing.WaitForSeconds(1f);
-            int port2 = PluginConfig.TcpPort;
-            IPAddress any = IPAddress.Parse(PluginConfig.IP);
+            int port2 =  GetMain.Config.TcpPort;
+            IPAddress any = IPAddress.Parse(GetMain.Config.IP);
             var ReceiveSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
             IPEndPoint ipendPoint = new IPEndPoint(any, port2);
             ReceiveSocket.Bind(new IPEndPoint(any, port2));
@@ -66,9 +74,9 @@ namespace SocketServer
                 {
                     string a = string.Empty;
                     a += $"服务器#1 - 查询成功 ";
-                    a += $"\r\n在线人数:{Player.GetPlayers().Count().ToString()}/{Server.MaxPlayers}";
-                    a += $"\r\n在线管理:{Player.GetPlayers().FindAll(x => x.RemoteAdminAccess).Count}人";
-                    a += $"\r\nIP:{Server.ServerIpAddress}";
+                    a += $"\r\n在线人数:{Player.List.Count().ToString()}/{Server.MaxPlayerCount}";
+                    a += $"\r\n在线管理:{Player.List.ToList().FindAll(x => x.RemoteAdminAccess).Count}人";
+                    a += $"\r\nIP:{Server.IpAddress}";
                     a += "\r\n查询时间" + DateTime.Now;
                     byte[] bytes = Encoding.UTF8.GetBytes(a);
                     socket.Send(bytes);
@@ -82,10 +90,6 @@ namespace SocketServer
         
         
         }
-
-
-        [PluginConfig] public static Config PluginConfig;
-
     }
 
 
